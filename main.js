f4101428f82ac8b0d97537f0040cc785953287e4
Pritish thakkar <t-prtha@microsoft.com>
2018-06-12 10:43:51 +0530

    added typewriter effect

---
+++ main.js
@@ -0,0 +1,31 @@
+var data = "Pritish Thakkar"
+var curr_idx = 0;
+var cursor = '|';
+var foo=1;
+
+function typewriter(){
+    var destination = document.getElementById("typeitbaby");
+    destination.innerHTML = data.substring(0, curr_idx);
+    if(data.length == curr_idx){
+        blink();
+        return;
+    }else{
+        destination.innerHTML = data.substring(0, curr_idx) + cursor;
+    }
+    curr_idx++;
+    setTimeout("typewriter()", 150);
+}
+
+function blink(){
+    var destination = document.getElementById("typeitbaby");
+    if(foo%2==0){
+        destination.innerHTML = data.substring(0, curr_idx) + cursor;
+        foo=1;
+    }else{
+        destination.innerHTML = data.substring(0, curr_idx) + ' ';
+        foo=0;
+    }
+    setTimeout("blink()", 400);
+}
+
+window.onload = typewriter;
